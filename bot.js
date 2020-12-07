869382381676468af026484ced3f1d3051947e52
mf-s <74371228+mf-s@users.noreply.github.com>
2020-12-07 13:51:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,32 +8,37 @@ var SteamUser = require('steam-user');
 var client = new SteamUser();
 var sleep = require('system-sleep');
 const readline = require('readline');
+const SteamTotp = require('steam-totp');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-rl.question('Username: ', function (accountname) {
-    rl.question('Password: ', function (password) {
-        rl.question('Message that you would like to send to your friends list: ', function (message) {
-            doLogin(accountname, password, message);
-            rl.close();
+rl.question('Username: ', function(accountname) {
+    rl.question('Password: ', function(password) {
+        rl.question('Guard code: ', function(guard) {
+            rl.question('Message that you would like to send to your friends list: ', function(message) {
+                doLogin(accountname, password, , guard, message);
+                rl.close();
+            });
         });
     });
 });
-function doLogin(accountname, password, message) {
+
+function doLogin(accountname, password, guard, message) {
     client.logOn({
         "accountName": accountname,
-        "password": password
+        "password": password,
+        twoFactorCode: SteamTotp.getAuthCode(guard),
     });
-    client.on('loggedOn', function (details) {
+    client.on('loggedOn', function(details) {
         console.log("Logged into Steam as " + client.steamID.getSteam3RenderedID());
         //console.log(details);
         client.setPersona(SteamUser.Steam.EPersonaState.Online);
     });
-    client.on('error', function (e) {
+    client.on('error', function(e) {
         console.log('ERROR: ' + e);
     });
-    client.on('friendsList', function () {
+    client.on('friendsList', function() {
         console.log('Number of friends to send a message to: ' + Object.keys(client.myFriends).length);
         var count = 0;
         for (var key in client.myFriends) {
@@ -47,7 +52,7 @@ function doLogin(accountname, password, message) {
             }
         }
     });
-    client.on('friendMessage', function (steamID, message) {
+    client.on('friendMessage', function(steamID, message) {
         console.log("Friend message from " + steamID.getSteam3RenderedID() + ": " + message);
     });
 }
